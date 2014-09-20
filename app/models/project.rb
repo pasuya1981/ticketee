de792791d1e710264984fbfd7dac7ad9294da148
pasuya1981 <pasuya1234@hotmail.com>
2014-09-20 18:04:52 +0800

    Add validation to ensure names are specified when creating projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Project < ActiveRecord::Base
+  validates_presence_of :name
 end
