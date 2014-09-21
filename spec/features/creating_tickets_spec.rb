6f16f7c46d7fe6d89155ae82021d652c3cbb7b5d
pasuya1981 <pasuya1234@hotmail.com>
2014-09-21 14:37:44 +0800

    Routine

---
+++ spec/features/creating_tickets_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature "Creating Tickets" do
+  
+  before do
+    FactoryGirl.create(:project, name: "Internet Explorer")
+    visit '/'
+    click_link "Internet Explorer"
+    click_link "New Ticket"
+  end
+
+  scenario "Creating a ticket" do
+    fill_in "Title", with: "Non-standard compliance"
+    fill_in "Description", with: "My page is ugly"
+    click_button "Create Ticket"
+
+    expect(page).to have_content("Ticket has been created.")
+  end
+
+  scenario "Creating a ticket withou valid attributes" do
+  	click_button 'Create Ticket'
+  	expect(page).to have_content("Ticket has not been created.")
+  	expect(page).to have_content("Title can't be blank")
+  	expect(page).to have_content("Description can't be blank")
+  end
+end
