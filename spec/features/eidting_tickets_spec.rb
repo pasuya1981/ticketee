6b5bc9d703d2e6950b9e33fa5918a867a72fc3fb
pasuya1981 <pasuya1234@hotmail.com>
2014-09-21 20:35:40 +0800

    We can now edit ticket

---
+++ spec/features/eidting_tickets_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature "Editing tickets" do
+  let!(:project) { FactoryGirl.create(:project) }
+  let!(:ticket) { FactoryGirl.create(:ticket, project: project) }
+
+  before do
+    visit '/'
+    click_link project.name
+    click_link ticket.title
+    click_link 'Edit Ticket'
+  end
+
+  scenario "Updating a ticket" do
+
+  	fill_in 'Title', with: 'Make it really shiny!'
+  	click_button 'Update Ticket'
+
+  	expect(page).to have_content "Ticket has been updated."
+    within("#ticket h2") do
+      expect(page).to have_content ticket.reload.title
+    end
+  end
+
+  scenario "Updating a ticket with invalid information" do
+    fill_in "Title", with: ""
+    click_button "Update Ticket"
+    expect(page).to have_content("Ticket has not been updated.")
+  end
+end
